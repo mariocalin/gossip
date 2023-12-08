3a79a0db90fa3c140299d0e3cb85332ef6cf860a
Mario <mariocalinsanchez@gmail.com>
2023-12-08 16:27:29 +0100

    delete dist

--- api/dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const server_1 = require("./server");
-// Create an application instance
-const application = new server_1.Api();
-// Run the application
-application.start();
